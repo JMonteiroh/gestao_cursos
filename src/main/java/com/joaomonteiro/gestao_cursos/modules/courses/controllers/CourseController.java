c648b700e0eef802778e1bfbfce9ca80cc770b3f
João Paulo <joao.paulomonteiro@hotmail.com>
2024-01-29 10:49:34 -0300

    creation of update and show routes

--- src/main/java/com/joaomonteiro/gestao_cursos/modules/courses/controllers/CourseController.java
+++ src/main/java/com/joaomonteiro/gestao_cursos/modules/courses/controllers/CourseController.java
@@ -1,11 +1,19 @@
 package com.joaomonteiro.gestao_cursos.modules.courses.controllers;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaomonteiro.gestao_cursos.modules.courses.entity.CourseEntity;
@@ -18,12 +26,49 @@ public class CourseController {
     @Autowired
     private CourseRepository courseRepository;
 
-    @PostMapping
-    public ResponseEntity<CourseEntity> create(@RequestBody CourseEntity courseEntity){
-        courseEntity.setActive(true);   
+    @PostMapping("/create")
+    public ResponseEntity<CourseEntity> create(@RequestBody CourseEntity courseEntity){  
 
         CourseEntity saveCourse = courseRepository.save(courseEntity);
 
         return new ResponseEntity<>(saveCourse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<List<CourseEntity>> show(
+        @RequestParam(required = false, defaultValue = "true") Boolean active
+    ) {
+        List<CourseEntity> courses;
+
+        if (active) {
+            courses = courseRepository.findByActiveTrue();
+        }else {
+            courses = courseRepository.findAll();
+        }
+        return new ResponseEntity<>(courses, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CourseEntity> update(@PathVariable Long id, @RequestBody CourseEntity updated) {
+        
+        Optional<CourseEntity> optionalCourse = courseRepository.findById(id);
+
+        if(optionalCourse.isPresent()) {
+            CourseEntity existingCourse = optionalCourse.get();
+
+            existingCourse.setName(updated.getName());
+            existingCourse.setCategory(updated.getCategory());
+
+            if (updated.isActive() != existingCourse.isActive()) {
+                existingCourse.setActive(updated.isActive());
+                existingCourse.setUpdated_at(new Date());
+            }
+
+            CourseEntity savedCourse = courseRepository.save(existingCourse);
+
+            return new ResponseEntity<>(savedCourse, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
